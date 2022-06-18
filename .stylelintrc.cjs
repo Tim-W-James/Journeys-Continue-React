c3b30f30f70e3491314fc3834621a970ceab48f4
TimJ <38764646+Tim-W-James@users.noreply.github.com>
2022-06-18 14:26:06 +1000

    Initial commit

---
+++ .stylelintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-prettier-scss",
+    "stylelint-config-idiomatic-order",
+  ],
+  rules: {
+    "unit-allowed-list": [
+      "em",
+      "rem",
+      "ch",
+      "vh",
+      "vw",
+      "vmin",
+      "vmax",
+      "px",
+      "%",
+      "s",
+      "deg",
+    ],
+    "color-named": "always-where-possible",
+    "color-no-hex": true,
+    "color-function-notation": "modern",
+    "length-zero-no-unit": true,
+    "font-weight-notation": "named-where-possible",
+    "declaration-no-important": true,
+    "alpha-value-notation": ["percentage", { exceptProperties: ["opacity"] }],
+  },
+  plugins: ["stylelint-order"],
+  ignoreFiles: ["./dist"],
+};
